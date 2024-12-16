272c7b896bff2659b1c0dbf2a3dbe6500f13fc01
alfreds22 <alfreds25@nycstudents.net>
2024-12-16 09:24:44 -0500

    .

--- basketball.py
+++ basketball.py
@@ -1,1 +1,14 @@
 import json
+with open("./teams.json", encoding="utf8") as teams_file:
+    teams = json.load(teams_file)
+search_name = input("Enter the name of teams to search for: ").strip().lower()
+found_team = []
+for teams in teams:
+    if teams['name'].lower() == search_name:
+        found_team.append(teams)
+if found_team:
+    print("Found teams:")
+    for teams in found_team:
+        print(f"- {teams['name']} (players: {teams['players']})")
+else:
+    print("No matching teams found. Make sure spelling is correct.")
