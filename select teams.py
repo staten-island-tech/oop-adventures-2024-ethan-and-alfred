a77abe7a608c531f842d747379faa6b4235dbe2c
ethanxie25 <randomized0938@gmail.com>
2024-12-17 09:30:48 -0500

    d

--- select teams.py
+++ select teams.py
@@ -1,15 +1,36 @@
-import os 
+import os
 import json
-with open ("./teams.json", encoding="utf8") as teams:
-    teams = json.load(teams)
+
+with open("./teams.json", encoding="utf8") as file:
+    teams_data = json.load(file)
+
 search_name = input("What team do you want to be? Choose the Nets, Lakers, Mavericks, Magic, Knicks or Warriors:").strip().lower()
+
 found_teams = []
-for teams in teams:
-    if teams == search_name:
-        found_teams.append(teams)
+
+for team in teams_data:
+    team_name = team.get('name', '').lower()  
+    if team_name == search_name:
+        found_teams.append(team)
+
 if found_teams:
-    print(['name'])
-    for teams in found_teams:
-        print(f'- is your current team.')
+    print(f"Found the following team:")
+    for team in found_teams:
+        print("This is your current team.")
 else:
     print("No team found. Choose the Nets, Lakers, Mavericks, Magic, Knicks or Warriors")
+
+search_players = input("Name a player from your team.")
+found_players = []
+
+for players in teams_data:
+    players_name = players.get('name', '').lower()  
+    if players_name == search_players:
+        found_players.append(players)
+if found_players:
+    print(f"Found your player:")
+    for player in found_players:
+        print(['players'])
+else:
+    print("No player found. Select another player from your team.")
+
