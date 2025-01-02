c62ccf31329c72a57da974365550b5f70d1fb869
alfreds22 <alfreds25@nycstudents.net>
2025-01-02 09:12:22 -0500

    .

--- select teams.py
+++ select teams.py
@@ -1,4 +1,4 @@
-teams = {Brooklyn_Nets = ["Dennis Schroder", "Cam Thomas", "Ben Simmons", "Dorian Finney-Smith", "Nic Claxton"],
+teams == {Brooklyn_Nets = ["Dennis Schroder", "Cam Thomas", "Ben Simmons", "Dorian Finney-Smith", "Nic Claxton"],
 Dallas_Mavericks = ["Luka Doncic", "Kyrie Irving", "Klay Thompson", "PJ Washington", "Dereck Lively II"],
 Golden_State_Warriors = ["Stephen Curry", "Brandin Podziemski", "Andrew Wiggins", "Draymond Green", "Trayce Jackson-Davis"],
 Los_Angeles_Lakers = ["Austin Reaves", "Dalton Knecht", "Rui Hachimura", "LeBron James", "Anthony Davis"],
